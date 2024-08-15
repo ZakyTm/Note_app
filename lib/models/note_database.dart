3f611bb5a21303dfc3f636b00fe3da900b0fa5e5
ZakyTm <zakaria.t149919@gmail.com>
2024-08-15 08:18:05 +0100

    Add isar_flutter_libs plugin to generated_plugins.cmake files

---
+++ lib/models/note_database.dart
@@ -0,0 +1,42 @@
+import 'package:isar/isar.dart';
+import 'package:note_app/models/note.dart';
+import 'package:path_provider/path_provider.dart';
+
+class NoteDataBase {
+  static late Isar isar;
+
+  // I N I T I A L I Z E   D A T A B A S E
+  static Future<void> initialize() async {
+    final dir = await getApplicationCacheDirectory();
+
+    isar = await Isar.open(
+      [NoteSchema],
+      directory: dir.path,
+    );
+  }
+
+  // list of notes
+  final List<Note> currentNotes = [];
+
+  // C R E A T E   N O T E - a note and save to db
+  Future<void> createNote(String textFromUser) async {
+    // create a new note object
+    final newNote = Note()..text = textFromUser;
+
+    // save the note to the db
+    await isar.writeTxn(() => isar.notes.put(newNote));
+
+    //re-read from the database
+  }
+
+  // R E A D  N O T E  - a notes from db
+  Future<void> readNotes() async {
+    List<Note> fetchNotes = await isar.notes.where().findAll();
+
+    currentNotes.clear();
+    currentNotes.addAll(fetchNotes);
+  }
+  //U P D A T E - a note in db
+
+  // D E L E T E - a note from the db
+}
