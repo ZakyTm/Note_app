ffc9fb2f51bb67a0809cd05a5ca829d7f8be36a9
ZakyTm <zakaria.t149919@gmail.com>
2024-08-15 09:04:02 +0100

    Refactor NotesPage to use StatefulWidget and add createNote functionality

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class NotesPage extends StatelessWidget {
+class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
 
   @override
+  State<NotesPage> createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  //text controller to access what the user typed
+  final textController = TextEditingController();
+
+  //create a note
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+           // create a button 
+           
+        ],
+      ),
+    );
+  }
+  // read Notes
+
+  // Update a note
+
+  //delete a note
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
