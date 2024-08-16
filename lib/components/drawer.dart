7ac96f56e2db508e4604259890f9d0ff6080343b
ZakyTm <zakaria.t149919@gmail.com>
2024-08-16 14:51:11 +0100

    feat: Add drawer tile for Settings page

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/components/drawer_tile.dart';
+import 'package:note_app/pages/settings_page.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
@@ -10,13 +12,29 @@ class MyDrawer extends StatelessWidget {
       child: Column(
         children: [
           //Header
-          DrawerHeader(
+          const DrawerHeader(
             child: Icon(Icons.note),
           ),
 
           // notes tile
-
+          DrawerTile(
+            title: "Notes",
+            leading: Icon(Icons.home),
+            onTap: () => Navigator.pop(context),
+          ),
           // Settings tile
+          DrawerTile(
+              title: "Settings",
+              leading: Icon(Icons.settings),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsPage(),
+                  ),
+                );
+              }),
         ],
       ),
     );
